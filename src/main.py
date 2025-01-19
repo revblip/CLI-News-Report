1213a5c78ca5947118a55b25ee10f957cb5878ee
revblip <>
2025-01-19 02:32:08 -0600

    Get list of story IDs from hacker-news API

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
+import requests
+
+base_url = "https://hacker-news.firebaseio.com/v0/"
+
+# Store a list of story IDs from the 500 newest stories on hacker-news
+def get_new_stories():
+    url = f"{base_url}/newstories.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        ids = response.json()
+        return ids
+    else:
+        print("Failed to retrieve data")
+
 def main():
-    print("Hello world")
+    new_story_ids = get_new_stories()
+    if new_story_ids:
+        print(new_story_ids[0])
 
 if __name__ == "__main__":
     main()
