54f613ef0c7e54086c56c181e3c4048c082faeb8
revblip <>
2025-01-19 03:45:19 -0600

    Get story information from ID and parse the html from the link to the web page for the story

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 base_url = "https://hacker-news.firebaseio.com/v0/"
 
-# Store a list of story IDs from the 500 newest stories on hacker-news
+# Return a list of story IDs from the 500 newest stories on hacker-news
 def get_new_stories():
     url = f"{base_url}/newstories.json"
     response = requests.get(url)
@@ -12,10 +13,31 @@ def get_new_stories():
     else:
         print("Failed to retrieve data")
 
+# Get information about a story given its ID
+# Returns a dictionary
+def get_story():
+    story_ids = get_new_stories()
+    if story_ids:
+        url = f"{base_url}/item/{story_ids[0]}.json"
+        response = requests.get(url)
+        if response.status_code == 200:
+            info = response.json()
+            return info
+        else:
+            print("Failed to retrieve data")
+
+
 def main():
     new_story_ids = get_new_stories()
     if new_story_ids:
         print(new_story_ids[0])
+    print(get_story())
+    testurl = "https://www.economist.com/finance-and-economics/2025/01/16/the-traitors-a-reality-tv-show-offers-a-useful-economics-lesson"
+    r = requests.get(testurl)
+    # Simplify the html of a given web page to just the readable text
+    clean_text = ' '.join(BeautifulSoup(r.content, "html.parser").stripped_strings)
+
+    print(clean_text)
 
 if __name__ == "__main__":
     main()
