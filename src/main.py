32d5ecfba08ff2d131bc6e9495336f592c4dae59
revblip <>
2025-01-19 19:12:41 -0600

    added get_story_content

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 
 base_url = "https://hacker-news.firebaseio.com/v0/"
 
-# Return a list of story IDs from the 500 newest stories on hacker-news
+"""
+Return a list of story IDs from the 500 newest stories on hacker-news
+"""
 def get_new_stories():
     url = f"{base_url}/newstories.json"
     response = requests.get(url)
@@ -13,9 +15,11 @@ def get_new_stories():
     else:
         print("Failed to retrieve data")
 
-# Get information about a story given its ID
-# Returns a dictionary
-def get_story():
+"""
+Get information about a story given its ID
+Returns a dictionary
+"""
+def get_story_dict():
     story_ids = get_new_stories()
     if story_ids:
         url = f"{base_url}/item/{story_ids[0]}.json"
@@ -26,18 +30,39 @@ def get_story():
         else:
             print("Failed to retrieve data")
 
+"""
+Get the html from the url given and convert it into clean text
+Returns a string
+"""
+def get_story_content(info):
+    if "url" not in info:
+        return "No URL present"
+    contentURL = info["url"]
+    request = requests.get(contentURL)
+    clean_text = ' '.join(BeautifulSoup(request.content, "html.parser").stripped_strings)
+    return clean_text
+
+
+def get_story_data(info):
+    title = info["title"]
+    author = info["by"]
+    date = info["time"]
+
+    print(f"Title = {title}")
+    print(f"Author = {author}")
+    print(f"Date = {date} (unix time)")
+
 
 def main():
     new_story_ids = get_new_stories()
     if new_story_ids:
         print(new_story_ids[0])
-    print(get_story())
-    testurl = "https://www.economist.com/finance-and-economics/2025/01/16/the-traitors-a-reality-tv-show-offers-a-useful-economics-lesson"
-    r = requests.get(testurl)
-    # Simplify the html of a given web page to just the readable text
-    clean_text = ' '.join(BeautifulSoup(r.content, "html.parser").stripped_strings)
+    print(get_story_dict())
+    print("\n")
+    print(get_story_content(get_story_dict()))
+    print("\n")
+    get_story_data(get_story_dict())
 
-    print(clean_text)
 
 if __name__ == "__main__":
     main()
